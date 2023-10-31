877f270bb3eaba78460ba75459f9a4ec9371947e
Raj <akankshashrivastav2103@gmail.com>
2023-10-31 22:15:29 +0530

    final updates

---
+++ app.py
@@ -0,0 +1,70 @@
+from importlib.resources import path
+from flask import Flask , render_template , request
+import numpy as np
+import pickle
+from pathlib import Path
+import joblib
+import os
+import pandas
+
+
+# location = 'C:\Users\Acer\OneDrive\Desktop\Heart_Disease_prediction-WebApp\Heart-Disease-Prediction-main'
+# fullpath = pandas.read_pickle('C:\Users\Acer\OneDrive\Desktop\Heart_Disease_prediction-WebApp\Heart-Disease-Prediction-main\hdp_model.pkl')
+
+# model_file = 'hdp_model.pkl'
+# model_path = os.path.join('C:', 'Users', 'Acer', 'OneDrive', 'Desktop', 'Heart_Disease_prediction-WebApp', 'Heart-Disease-Prediction-main', model_file)
+
+model_file = 'hdp_model.pkl'
+model_path = 'C:/Users/Acer/OneDrive/Desktop/Heart_Disease_prediction-WebApp/Heart-Disease-Prediction-main/' + model_file
+
+
+app = Flask(__name__)
+#model = pickle.load(open(fullpath, 'rb'))
+model = joblib.load(model_path)
+
+@app.route("/",)
+def hello():
+    return render_template("index.html")
+
+
+@app.route("/detail", methods = ["POST"])
+def submit():
+    # Html to py
+    if request.method == "POST":
+        name = request.form["Username"]
+
+    return render_template("detail.html", n = name)
+
+
+@app.route('/predict', methods = ["POST"])
+def predict():
+    if request.method == "POST":
+        
+        age = int(request.form['age'])
+        sex = int(request.form['sex'])
+        cp = int(request.form['cp'])
+        trestbps = int(request.form['trestbps'])
+        chol = int(request.form['chol'])
+        fbs = int(request.form['fbs'])
+        restecg = int(request.form['restecg'])
+        thalach = int(request.form['thalach'])
+        exang = int(request.form['exang'])
+        oldpeak = float(request.form['oldpeak'])
+        slope = int(request.form['slope'])
+        ca = int(request.form['ca'])
+        thal = int(request.form['thal'])
+
+        values = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
+        prediction = model.predict(values)
+        
+
+    
+
+
+        return render_template('predict.html', prediction=prediction)
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+    
